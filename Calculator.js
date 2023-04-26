2335494fa2b96472268b334f8d497e112b637276
joaojv90 <joao.jaramillo@gmail.com>
2023-04-26 09:09:04 -0500

    class Calculator

---
+++ Calculator.js
@@ -0,0 +1,17 @@
+class Calculator {
+    sumar(num1, num2){
+        return num1 + num2;
+    }
+
+    resta(num1, num2){
+        return num1 - num2;
+    }
+
+    multiplicar(num1, num2){
+        return num1 * num2;
+    }
+
+    dividir(num1, num2){
+        return num1 / num2;
+    }
+}
