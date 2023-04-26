aa2198f19baab894ed634f4a29bcf4e5e44b98e8
joaojv90 <joao.jaramillo@gmail.com>
2023-04-25 21:59:47 -0500

    created body calculator

---
+++ index.js
@@ -0,0 +1,5 @@
+const displayValorAnterior = document.getElementById('valor_anterior');
+const displayValorActual = document.getElementById('valor_actual');
+const numbersButtons = document.querySelectorAll('.number');
+const operatorsButtons = document.querySelectorAll('.operators');
+
